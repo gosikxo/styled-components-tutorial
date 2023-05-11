5a18256a3c9b342470b90ed8683860d66d607afb
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-05-11 13:47:39 +0100

    Add two styled components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-function App() {
-  return <div className="App"></div>;
+import { Button } from "./components/Button.style";
+import { AppContainer } from "./components/Container.style";
+
+function App () {
+  return (
+    <AppContainer>
+      <Button backgroundColor="violet"> Click me </Button>
+      <Button backgroundColor="turquoise"> Click me </Button>
+    </AppContainer>
+  )
 }
 
 export default App;
